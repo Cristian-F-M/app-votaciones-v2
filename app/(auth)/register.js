2f1360c870425cfe6048ef08d0e2f8e3a3a23ded
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-08-04 13:03:29 -0500

    chore: add required validation to inputs, add reference to all inputs and navigate to required input

--- app/(auth)/register.js
+++ app/(auth)/register.js
@@ -9,16 +9,25 @@ import {
 } from 'react-native'
 import { Screen } from '../../components/Screen'
 import LogoSena from '../../icons/Logo'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { doFetch, getItemStorage, METHODS, setItemStorage } from '../../lib/api'
 import { Picker } from '@react-native-picker/picker'
 import { Stack } from 'expo-router'
+import { scrollSmooth } from '../../lib/scrollSmooth'
 
 export default function Register() {
   const [Color, setColor] = useState('')
   const [errors, setErrors] = useState({})
   const [typeDocumentCode, setTypeDocumentCode] = useState('CedulaCiudadania')
   const [isLoading, setIsLoading] = useState(false)
+
+  const scrollViewRef = useRef(null)
+  const nameRef = useRef(null)
+  const lastnameRef = useRef(null)
+  const documentRef = useRef(null)
+  const phoneRef = useRef(null)
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
 
   const [name, setName] = useState('')
   const [lastname, setLastname] = useState('')
@@ -28,6 +37,43 @@ export default function Register() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+
+  function handleClickRegister() {
+    let localyErrors = {}
+
+    if (name.trim() === '')
+      localyErrors = { ...localyErrors, name: 'Campo requerido' }
+    if (lastname.trim() === '')
+      localyErrors = { ...localyErrors, lastname: 'Campo requerido' }
+    if (document.trim() === '')
+      localyErrors = { ...localyErrors, document: 'Campo requerido' }
+    if (phone.trim() === '')
+      localyErrors = { ...localyErrors, phone: 'Campo requerido' }
+    if (email.trim() === '')
+      localyErrors = { ...localyErrors, email: 'Campo requerido' }
+
+    if (password.trim() === '')
+      localyErrors = { ...localyErrors, password: 'Campo requerido' }
+    if (passwordConfirm.trim() === '')
+      localyErrors = { ...localyErrors, passwordConfirm: 'Campo requerido' }
+    if (password !== passwordConfirm)
+      localyErrors = {
+        ...localyErrors,
+        passwordConfirm: 'Las contraseñas no coinciden',
+      }
+    setErrors(localyErrors)
+
+    if (localyErrors.name) return scrollSmooth(nameRef, scrollViewRef)
+    if (localyErrors.lastname) return scrollSmooth(lastnameRef, scrollViewRef)
+    if (localyErrors.document) return scrollSmooth(documentRef, scrollViewRef)
+    if (localyErrors.phone) return scrollSmooth(phoneRef, scrollViewRef)
+    if (localyErrors.email) return scrollSmooth(emailRef, scrollViewRef)
+    if (localyErrors.password) return scrollSmooth(passwordRef, scrollViewRef)
+
+    if (localyErrors.length > 0) return
+
+    async function Register() {}
+  }
 
   useEffect(() => {
     async function getConfigs() {
@@ -73,7 +119,10 @@ export default function Register() {
           ),
         }}
       />
-      <ScrollView className="p-5 flex-1">
+      <ScrollView
+        className="p-5 flex-1"
+        ref={scrollViewRef}
+      >
         <View className="flex-1 justify-center">
           <View className="flex-col items-center gap-4 mb-10">
             <Text className="text-4xl text-center tracking-widest">
@@ -82,7 +131,10 @@ export default function Register() {
           </View>
 
           <View>
-            <View style={styles.inputContainer}>
+            <View
+              style={styles.inputContainer}
+              ref={nameRef}
+            >
               <Text style={styles.label}>
                 Nombre
                 <Text className="text-red-500"> *</Text>
@@ -96,9 +148,15 @@ export default function Register() {
                 }}
                 placeholder="Luna Sophia"
               />
+              {!!errors.name && (
+                <Text style={styles.errorMessage}>{errors.name}</Text>
+              )}
             </View>
             {/*  */}
-            <View style={styles.inputContainer}>
+            <View
+              style={styles.inputContainer}
+              ref={lastnameRef}
+            >
               <Text style={styles.label}>
                 Apellido
                 <Text className="text-red-500"> *</Text>
@@ -112,6 +170,9 @@ export default function Register() {
                 }}
                 placeholder="Smith Miller"
               />
+              {!!errors.lastname && (
+                <Text style={styles.errorMessage}>{errors.lastname}</Text>
+              )}
             </View>
             {/*  */}
             <View style={styles.inputContainer}>
@@ -146,7 +207,10 @@ export default function Register() {
               </View>
             </View>
             {/*  */}
-            <View style={styles.inputContainer}>
+            <View
+              style={styles.inputContainer}
+              ref={documentRef}
+            >
               <Text style={styles.label}>
                 Documento
                 <Text className="text-red-500"> *</Text>
@@ -158,11 +222,18 @@ export default function Register() {
                   setDocument(t)
                   setErrors({ ...errors, document: null })
                 }}
+                keyboardType="numeric"
                 placeholder="123456789"
               />
+              {!!errors.document && (
+                <Text style={styles.errorMessage}>{errors.document}</Text>
+              )}
             </View>
             {/*  */}
-            <View style={styles.inputContainer}>
+            <View
+              style={styles.inputContainer}
+              ref={phoneRef}
+            >
               <Text style={styles.label}>
                 Telefono
                 <Text className="text-red-500"> *</Text>
@@ -174,11 +245,18 @@ export default function Register() {
                   setPhone(t)
                   setErrors({ ...errors, phone: null })
                 }}
+                keyboardType="phone-pad"
                 placeholder="3512345678"
               />
+              {!!errors.phone && (
+                <Text style={styles.errorMessage}>{errors.phone}</Text>
+              )}
             </View>
             {/*  */}
-            <View style={styles.inputContainer}>
+            <View
+              style={styles.inputContainer}
+              ref={emailRef}
+            >
               <Text style={styles.label}>
                 Correo electronico
                 <Text className="text-red-500"> *</Text>
@@ -186,15 +264,22 @@ export default function Register() {
               <TextInput
                 style={[styles.input, styles.inputText]}
                 value={email}
+                keyboardType="email-address"
                 onChangeText={t => {
                   setEmail(t)
                   setErrors({ ...errors, email: null })
                 }}
                 placeholder="lunasophia@gmail.com"
               />
+              {!!errors.email && (
+                <Text style={styles.errorMessage}>{errors.email}</Text>
+              )}
             </View>
             {/*  */}
-            <View style={styles.inputContainer}>
+            <View
+              style={styles.inputContainer}
+              ref={passwordRef}
+            >
               <Text style={styles.label}>
                 Contraseña
                 <Text className="text-red-500"> *</Text>
@@ -209,6 +294,9 @@ export default function Register() {
                 }}
                 placeholder="*********"
               />
+              {!!errors.password && (
+                <Text style={styles.errorMessage}>{errors.password}</Text>
+              )}
             </View>
             {/*  */}
             <View style={styles.inputContainer}>
@@ -226,6 +314,11 @@ export default function Register() {
                 }}
                 placeholder="*********"
               />
+              {!!errors.passwordConfirm && (
+                <Text style={styles.errorMessage}>
+                  {errors.passwordConfirm}
+                </Text>
+              )}
             </View>
           </View>
 
@@ -239,6 +332,7 @@ export default function Register() {
                 backgroundColor: `${Color}${isLoading ? '80' : ''}`,
                 color: `#000000${isLoading ? '80' : ''}`,
               }}
+              onPress={!isLoading ? handleClickRegister : null}
             >
               <Text className="text-lg">Registrar</Text>
               {isLoading && (
