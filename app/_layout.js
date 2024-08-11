f169317da00b48732c6547757bc195274438535d
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-08-11 16:11:05 -0500

    chore: Use ConfigProvider

--- app/_layout.js
+++ app/_layout.js
@@ -1,13 +1,16 @@
 import { Stack } from 'expo-router'
 import { View } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { ConfigProvider } from '../context/config.js'
 
 export default function Layout() {
   return (
-    <SafeAreaProvider>
-      <View className="flex-1">
-        <Stack screenOptions={{ headerShown: false }} />
-      </View>
-    </SafeAreaProvider>
+    <ConfigProvider>
+      <SafeAreaProvider>
+        <View className="flex-1">
+          <Stack screenOptions={{ headerShown: false }} />
+        </View>
+      </SafeAreaProvider>
+    </ConfigProvider>
   )
 }
--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
 import { View } from 'react-native'
 import { Main } from '../components/Main'
+import { ConfigProvider } from '../context/config'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <StatusBar style="auto" />
-      <Main />
-    </View>
+    <ConfigProvider>
+      <View className="flex-1 items-center justify-center">
+        <StatusBar style="auto" />
+        <Main />
+      </View>
+    </ConfigProvider>
   )
 }
