6c9ac31690b91b65093e87033e4e49bf0bb68197
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-08-11 16:25:01 -0500

    chore: Add AlertNotificationRoot

--- app/_layout.js
+++ app/_layout.js
@@ -2,14 +2,17 @@ import { Stack } from 'expo-router'
 import { View } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ConfigProvider } from '../context/config.js'
+import { AlertNotificationRoot } from 'react-native-alert-notification'
 
 export default function Layout() {
   return (
     <ConfigProvider>
       <SafeAreaProvider>
-        <View className="flex-1">
-          <Stack screenOptions={{ headerShown: false }} />
-        </View>
+        <AlertNotificationRoot>
+          <View className="flex-1">
+            <Stack screenOptions={{ headerShown: false }} />
+          </View>
+        </AlertNotificationRoot>
       </SafeAreaProvider>
     </ConfigProvider>
   )
