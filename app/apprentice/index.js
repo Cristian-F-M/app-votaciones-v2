69a9e44c95670cae0a55d704cda1ea9504282b2f
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-09-10 19:56:51 -0500

    refactor: Remove unuse code

--- app/apprentice/index.js
+++ app/apprentice/index.js
@@ -1,4 +1,4 @@
-import { Pressable, Text, View } from 'react-native'
+import { Button, Pressable, Text, View } from 'react-native'
 import { Screen } from '../../components/Screen'
 import { doFetch, METHODS, removeItemStorage } from '../../lib/api'
 import { ALERT_TYPE, Toast } from 'react-native-alert-notification'
@@ -9,22 +9,6 @@ import { Shadow } from 'react-native-shadow-2'
 
 export default function Index() {
   const router = useRouter()
-
-  async function handleClickLogout() {
-    const url = `${process.env.EXPO_PUBLIC_API_URL}/Logout`
-    const res = await doFetch({ url, method: METHODS.POST })
-
-    if (res.error) {
-      return Toast.show({
-        type: ALERT_TYPE.DANGER,
-        title: 'Error',
-        textBody: `${res.error}, please try again`,
-      })
-    }
-
-    removeItemStorage({ name: 'token' })
-    router.replace('/')
-  }
 
   return (
     <View className="flex-1 bg-white">
