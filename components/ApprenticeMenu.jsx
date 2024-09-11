0e2be8d14deec46b7a36a66390c28d8bf3bdd95e
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-09-10 19:59:31 -0500

    chore: Add button and function to logout

--- components/ApprenticeMenu.jsx
+++ components/ApprenticeMenu.jsx
@@ -3,8 +3,27 @@ import { Screen } from './Screen.jsx'
 import { StatusBar } from 'expo-status-bar'
 import { Profile } from './Profile.jsx'
 import { Shadow } from 'react-native-shadow-2'
+import { doFetch, METHODS, removeItemStorage } from '../lib/api.js'
+import { ALERT_TYPE, Toast } from 'react-native-alert-notification'
+import { router } from 'expo-router'
 
 export function ApprenticeMenu() {
+  async function handleClickLogout() {
+    const url = `${process.env.EXPO_PUBLIC_API_URL}/Logout`
+    const res = await doFetch({ url, method: METHODS.POST })
+
+    if (res.error) {
+      return Toast.show({
+        type: ALERT_TYPE.DANGER,
+        title: 'Error',
+        textBody: `${res.error}, please try again`,
+      })
+    }
+
+    removeItemStorage({ name: 'token' })
+    router.replace('/')
+  }
+
   return (
     <Screen>
       <StatusBar
@@ -16,6 +35,12 @@ export function ApprenticeMenu() {
 
         <View className="py-4">
           <Text>Menu</Text>
+          <Pressable
+            className="mt-4 bg-red-400 py-4 px-2"
+            onPress={handleClickLogout}
+          >
+            <Text className="text-base text-center">Cerrar Sesión</Text>
+          </Pressable>
         </View>
       </ScrollView>
     </Screen>
