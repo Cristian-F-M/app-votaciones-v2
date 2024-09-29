6cb7bdf00c272e24faf1deac0622c06fa42fc6b0
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-09-28 22:02:15 -0500

    chore: Add config link

--- components/ApprenticeMenu.jsx
+++ components/ApprenticeMenu.jsx
@@ -5,9 +5,9 @@ import { Profile } from './Profile.jsx'
 import { Shadow } from 'react-native-shadow-2'
 import { doFetch, METHODS, removeItemStorage } from '../lib/api.js'
 import { ALERT_TYPE, Toast } from 'react-native-alert-notification'
-import { router } from 'expo-router'
+import { Link, router } from 'expo-router'
 
-export function ApprenticeMenu() {
+export function ApprenticeMenu({ setMenuIsVisible }) {
   async function handleClickLogout() {
     const url = `${process.env.EXPO_PUBLIC_API_URL}/Logout`
     const res = await doFetch({ url, method: METHODS.POST })
@@ -24,6 +24,11 @@ export function ApprenticeMenu() {
     router.replace('/')
   }
 
+  function handleClickConfig() {
+    setMenuIsVisible(false)
+    router.push('apprentice/config/')
+  }
+
   return (
     <Screen>
       <StatusBar
@@ -33,8 +38,14 @@ export function ApprenticeMenu() {
       <ScrollView className="flex-1 bg-gray-[#e0e2e4]">
         <Profile />
 
-        <View className="py-4">
-          <Text>Menu</Text>
+        <View className="py-4 flex flex-col gap-y-2">
+          <Pressable
+            className="mt-4 bg-blue-400 py-4 px-2"
+            onPress={handleClickConfig}
+          >
+            <Text className="text-base text-center">Configuraciones</Text>
+          </Pressable>
+          {/*  */}
           <Pressable
             className="mt-4 bg-red-400 py-4 px-2"
             onPress={handleClickLogout}
