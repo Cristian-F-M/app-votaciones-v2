8bdf2833f490ad63b69ccd56301188c3a91a4dba
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-10-05 17:39:28 -0500

    refactor: Change InsetShadow to Shadow component

--- components/CardConfig.jsx
+++ components/CardConfig.jsx
@@ -3,13 +3,14 @@ import { useConfig } from '../context/config'
 import { findConfig } from '../lib/config'
 import { HorizontalSeparator } from './HorizontalSeparator'
 import InsetShadow from 'react-native-inset-shadow'
+import { Shadow } from 'react-native-shadow-2'
 
 export function CardConfig({ children, title }) {
   const { config } = useConfig()
   const { value } = findConfig({ configs: config, code: 'Color' })
 
   return (
-    <InsetShadow>
+    <Shadow className="">
       <View className="bg-gray-200/60 p-2">
         <Text
           className="text-lg"
@@ -20,6 +21,6 @@ export function CardConfig({ children, title }) {
         <HorizontalSeparator />
         <View className="mt-1">{children}</View>
       </View>
-    </InsetShadow>
+    </Shadow>
   )
 }
