fee1f4e1b21f9c4e622fdd66173277005d949751
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-12-02 18:42:16 -0500

    chore: Enhance voting functionality in Apprentice index with state management and dynamic rendering of winner and vote components

--- app/apprentice/index.js
+++ app/apprentice/index.js
@@ -4,20 +4,44 @@ import { doFetch, METHODS, removeItemStorage } from '../../lib/api'
 import { ALERT_TYPE, Toast } from 'react-native-alert-notification'
 import { useRouter } from 'expo-router'
 import SideMenu from '@chakrahq/react-native-side-menu'
-import { useState } from 'react'
-import { Shadow } from 'react-native-shadow-2'
+import { useCallback, useEffect, useState } from 'react'
+import { useConfig } from '../../context/config'
+import { findConfig } from '../../lib/config'
+import { ApprenticeWinner } from '../../components/ApprenticeWinner'
+import { Vote } from '../../components/Vote'
+import { ThereIsNoVote } from '../../components/ThereIsNoVote'
 
 export default function Index() {
-  const router = useRouter()
+  const url = process.env.EXPO_PUBLIC_API_URL
+  const [isVoteFinished, setIsVoteFinished] = useState(false)
+  const [thereIsVote, setThereIsVote] = useState(false)
+
+  const getLastVote = useCallback(async () => {
+    doFetch({ url: `${url}/vote/`, method: METHODS.GET }).then(data => {
+      if (data.error) return
+
+      const { vote } = data
+
+      if (!vote) return setThereIsVote(false)
+      if (new Date() > vote.endDate) return setIsVoteFinished(true)
+
+      setThereIsVote(true)
+      setIsVoteFinished(false)
+    })
+  }, [url])
+
+  useEffect(() => {
+    getLastVote()
+  }, [getLastVote])
+
+  const showVote = !isVoteFinished && thereIsVote
+  const showWinner = isVoteFinished && thereIsVote
 
   return (
-    <View className="flex-1 bg-white">
-      <Text className="mb-10">Aprenttice</Text>
-      <Shadow>
-        <View>
-          <Text>Aprenttice</Text>
-        </View>
-      </Shadow>
-    </View>
+    <>
+      {showWinner && <ApprenticeWinner />}
+      {showVote && <Vote />}
+      {!thereIsVote && <ThereIsNoVote />}
+    </>
   )
 }
