00125e6340013dcc8489b64e2a6d044363d16e63
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-12-03 18:42:21 -0500

    feat: Move notification handler to _layout.js and remove it from App.js for better organization and maintainability

--- App.js
+++ App.js
@@ -2,17 +2,8 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import { Main } from './components/Main'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import * as Notifications from 'expo-notifications'
 
 export default function App() {
-  Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-      shouldShowAlert: true,
-      shouldPlaySound: true,
-      shouldSetBadge: true,
-    }),
-  })
-
   return (
     <SafeAreaProvider>
       <View className="flex-1 items-center justify-center">
--- app/_layout.js
+++ app/_layout.js
@@ -4,6 +4,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ConfigProvider } from '../context/config.js'
 import { AlertNotificationRoot } from 'react-native-alert-notification'
 
+import * as Notifications from 'expo-notifications'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+})
+
 export default function Layout() {
   return (
     <ConfigProvider>
