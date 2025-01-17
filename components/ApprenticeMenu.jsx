1f9b2f82c29a13584bc2aa00eefd28c364b7be6a
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-01-17 15:05:41 -0500

    feat: Add ApprenticeMenu component

--- components/ApprenticeMenu.jsx
+++ components/ApprenticeMenu.jsx
@@ -6,8 +6,12 @@ import { Shadow } from 'react-native-shadow-2'
 import { doFetch, METHODS, removeItemStorage } from '../lib/api.js'
 import { ALERT_TYPE, Toast } from 'react-native-alert-notification'
 import { Link, router } from 'expo-router'
+import { useUser } from '../context/user.js'
+import { useEffect } from 'react'
 
 export function ApprenticeMenu({ setMenuIsVisible }) {
+  const { user } = useUser()
+
   async function handleClickLogout() {
     const url = `${process.env.EXPO_PUBLIC_API_URL}/Logout`
     const res = await doFetch({ url, method: METHODS.POST })
@@ -29,6 +33,11 @@ export function ApprenticeMenu({ setMenuIsVisible }) {
     router.push('apprentice/config/')
   }
 
+  function handleClickCandidateProfile() {
+    setMenuIsVisible(false)
+    router.navigate('candidate/profile/')
+  }
+
   return (
     <Screen>
       <StatusBar
@@ -39,6 +48,14 @@ export function ApprenticeMenu({ setMenuIsVisible }) {
         <Profile />
 
         <View className="py-4 flex flex-col gap-y-2">
+          {user?.roleUser.code === 'Candidate' && (
+            <Pressable
+              className="mt-4 bg-green-400 py-4 px-2"
+              onPress={handleClickCandidateProfile}
+            >
+              <Text className="text-base text-center">Perfil de candidato</Text>
+            </Pressable>
+          )}
           <Pressable
             className="mt-4 bg-blue-400 py-4 px-2"
             onPress={handleClickConfig}
