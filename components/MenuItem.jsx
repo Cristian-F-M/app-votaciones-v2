f0e9fb2418db927485bb1ed8d00dc67258b3b683
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-01-31 15:35:10 -0500

    feat: Add MenuItem component for reusable menu options with customizable text and pressable functionality

---
+++ components/MenuItem.jsx
@@ -0,0 +1,15 @@
+import { Pressable, Text } from 'react-native'
+
+export function MenuItem({ text, onPress, pressableClass = '', children }) {
+  return (
+    <>
+      <Pressable
+        className={`mt-3 bg-gray-400 py-4 px-2 rounded-sm ${pressableClass}`}
+        onPress={onPress}
+      >
+        {children && children}
+        {!children && <Text className="text-base text-center">{text}</Text>}
+      </Pressable>
+    </>
+  )
+}
