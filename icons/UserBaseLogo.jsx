3a6295a507657e14fb02c6babb971bab95151e44
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-01-31 11:37:05 -0500

    feat: Increase UserBaseLogo font size from 2xl to 3xl for better visibility and overall design consistency

--- icons/UserBaseLogo.jsx
+++ icons/UserBaseLogo.jsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 export function UserBaseLogo({ letter }) {
   return (
     <View className="w-full h-full flex justify-center items-center">
-      <Text className="text-2xl font-semibold">{letter}</Text>
+      <Text className="text-3xl font-semibold">{letter}</Text>
     </View>
   )
 }
