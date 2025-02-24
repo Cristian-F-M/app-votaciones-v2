7094858a5781ecac8bf6792820074ade07b67d9b
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-02-24 16:55:23 -0500

    feat: Create reusable Input component with text and select types, including error handling and customization options

---
+++ components/Input.jsx
@@ -0,0 +1,86 @@
+import { Picker } from '@react-native-picker/picker'
+import { Text, TextInput, View } from 'react-native'
+
+export const INPUT_TYPES = {
+  TEXT: 'text',
+  SELECT: 'select',
+}
+
+export function Input({
+  value,
+  placeholder,
+  onChange,
+  keyboardType = 'default',
+  type = INPUT_TYPES.TEXT,
+  label = 'no-label',
+  classNameInput = '',
+  errors = {},
+  inputRefName = '',
+  innerRef = null,
+  selectedValue = '',
+  dropdownIconRippleColor = 'white',
+  onValueChange = () => {},
+  selectText = '',
+  items = [],
+  disabled = false,
+}) {
+  return (
+    <View
+      className="mb-4 relative"
+      ref={innerRef}
+    >
+      <Text className="text-black mb-1 text-base">{label}</Text>
+      {type === INPUT_TYPES.TEXT && (
+        <TextInput
+          className={`border rounded-lg h-14 w-full text-base px-3 ${classNameInput}`}
+          value={value.value}
+          placeholder={placeholder}
+          onChangeText={text => {
+            value.setValue(text)
+            errors.setErrors(prev => ({ ...prev, [inputRefName]: null }))
+          }}
+          keyboardType={keyboardType}
+          editable={!disabled}
+        />
+      )}
+
+      {type === INPUT_TYPES.SELECT && (
+        <View
+          className={`border rounded-lg h-14 w-full text-base px-3 ${classNameInput}`}
+        >
+          <Picker
+            selectedValue={selectedValue}
+            dropdownIconRippleColor={dropdownIconRippleColor}
+            mode="modal"
+            prompt={selectText}
+            onValueChange={(itemValue, itemIndex) =>
+              onValueChange(itemValue, itemIndex)
+            }
+            enabled={!disabled}
+          >
+            {!items || items.length === 0 ? (
+              <Picker.Item
+                label={'Loading...'}
+                value={0}
+              />
+            ) : (
+              items.map(item => {
+                return (
+                  <Picker.Item
+                    key={item.id}
+                    label={item.name}
+                    value={item.code}
+                  />
+                )
+              })
+            )}
+          </Picker>
+        </View>
+      )}
+
+      {errors.errors && errors.errors[inputRefName] && (
+        <Text className="text-red-600">{errors.errors[inputRefName]}</Text>
+      )}
+    </View>
+  )
+}
