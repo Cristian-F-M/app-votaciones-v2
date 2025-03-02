04964fe71f8201bca693d76e1a923b3d9be83aef
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-02 16:07:11 -0500

    fix: Rename color prop to backgroundColor in StyledPressable for improved clarity and consistent usage across components

--- components/StyledPressable.jsx
+++ components/StyledPressable.jsx
@@ -2,7 +2,7 @@ import { ActivityIndicator, Pressable, Text } from 'react-native'
 import { styled } from 'nativewind'
 
 export function StyledPressable({
-  color = 'white',
+  backgroundColor = 'white',
   text = 'no-text',
   pressableClass = '',
   textClassName = '',
@@ -15,7 +15,7 @@ export function StyledPressable({
   return (
     <StyledPressable
       className={`px-2 py-3 rounded-lg w-full active:opacity-70 flex-row justify-center items-center ${isLoading ? 'opacity-70' : ''} ${pressableClass}`}
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor }}
       onPress={!isLoading ? onPress : null}
       disabled={isLoading}
     >
