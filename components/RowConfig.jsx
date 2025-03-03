066a7317372fb3702bf3f241e3f690901de4b98b
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-03 16:20:57 -0500

    fix: Enhance RowConfig component with switch functionality and dynamic rendering for better user interaction and configurability

--- components/RowConfig.jsx
+++ components/RowConfig.jsx
@@ -1,11 +1,54 @@
-import { View } from "react-native";
+import { Switch, Text, View } from 'react-native'
+import { useConfig } from '../context/config'
+import { findConfig } from '../lib/config'
 
-export function RowConfig({ children }) {
+export function RowConfig({
+  label,
+  useSwitch,
+  switchValue = {},
+  falseColor = '#767577',
+  trueColor = '',
+  onChange = () => {},
+  children,
+  configContent,
+  disabled = false,
+}) {
+  const { config } = useConfig()
+  const configColor = findConfig({ configs: config, code: 'Color' }).value
+  const color = trueColor || configColor
+
   return (
     <>
       <View className="flex flex-row justify-between items-center w-full">
-        {children}
+        {children && children}
+        {!children && (
+          <>
+            <View>
+              <Text style={{ color: disabled ? '#99a1af' : '#000' }}>
+                {label}
+              </Text>
+            </View>
+            {useSwitch && (
+              <View>
+                <Switch
+                  trackColor={{
+                    false: disabled ? '#99a1af' : falseColor,
+                    true: color,
+                  }}
+                  thumbColor="white"
+                  onValueChange={() => {
+                    switchValue.setValue(!switchValue.value)
+                  }}
+                  onChange={onChange}
+                  value={!disabled ? switchValue.value : false}
+                  disabled={disabled}
+                />
+              </View>
+            )}
+            {!useSwitch && configContent}
+          </>
+        )}
       </View>
     </>
-  );
+  )
 }
