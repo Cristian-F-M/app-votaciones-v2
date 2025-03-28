e8ee4a21bc784cfe1ed95b24af1b43e538983678
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-28 18:45:07 -0500

    feat: Integrate LoadingWinner component to handle notifications during voting results processing

--- app/apprentice/index.js
+++ app/apprentice/index.js
@@ -3,11 +3,13 @@ import { useCallback, useEffect, useState } from 'react'
 import { ApprenticeWinner } from '../../components/ApprenticeWinner'
 import { Vote } from '../../components/Vote'
 import { ThereIsNoVote } from '../../components/ThereIsNoVote'
+import { LoadingWinner } from '../../components/LoadingWinner'
 
 export default function Index() {
   const url = process.env.EXPO_PUBLIC_API_URL
   const [isVoteFinished, setIsVoteFinished] = useState(false)
   const [thereIsVote, setThereIsVote] = useState(false)
+  const [arrivedTime, setArrivedTime] = useState(true)
 
   const getLastVote = useCallback(async () => {
     doFetch({ url: `${url}/vote/`, method: METHODS.GET }).then(data => {
@@ -20,7 +22,8 @@ export default function Index() {
       if (new Date().getTime() > new Date(vote.startDate).getTime())
         setThereIsVote(true)
       if (new Date().getTime() > new Date(vote.endDate).getTime())
-        setIsVoteFinished(true)
+        setArrivedTime(true)
+      if (vote?.isFinished) setIsVoteFinished(true)
     })
   }, [url])
 
@@ -28,14 +31,15 @@ export default function Index() {
     getLastVote()
   }, [getLastVote])
 
-  const showVote = !isVoteFinished && thereIsVote
-  const showWinner = isVoteFinished && thereIsVote
+  const showVote = !isVoteFinished && thereIsVote && !arrivedTime
+  const showWinner = isVoteFinished && thereIsVote && arrivedTime
 
   return (
     <>
       {showWinner && <ApprenticeWinner />}
       {showVote && <Vote />}
       {!thereIsVote && <ThereIsNoVote />}
+      {arrivedTime && !isVoteFinished && <LoadingWinner />}
     </>
   )
 }
---
+++ components/LoadingWinner.jsx
@@ -0,0 +1,116 @@
+import { Linking, Text, ToastAndroid, View } from 'react-native'
+import BarChart from '../icons/BarChart'
+import { StyledPressable } from './StyledPressable'
+import Bell from '../icons/Bell'
+import { Modalize } from 'react-native-modalize'
+import { useCallback, useRef, useState } from 'react'
+import { activateNotifications } from '../lib/config'
+
+export function LoadingWinner() {
+  const color = '#ff6719'
+  const [isNotificationsActive, setIsNotificationsActive] = useState(false)
+  const modalizeRef = useRef(null)
+  const handleClickOpenSettings = useCallback(() => {
+    Linking.openSettings()
+    modalizeRef.current.close()
+  }, [])
+
+  const checkPermissions = useCallback(async e => {
+    const isActivated = await activateNotifications()
+
+    if (isActivated) {
+      ToastAndroid.show(
+        'Ya tienes las notificaciones activadas',
+        ToastAndroid.LONG,
+      )
+      setIsNotificationsActive(true)
+      return
+    }
+
+    if (!isActivated) openModalize(e)
+  }, [])
+
+  const openModalize = e => {
+    e.persist()
+    modalizeRef.current.open()
+  }
+
+  const handleClickdDismissModalize = useCallback(() => {
+    modalizeRef.current.close()
+  }, [])
+
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Modalize
+        ref={modalizeRef}
+        adjustToContentHeight
+        modalStyle={{ paddingVertical: 15, paddingHorizontal: 20 }}
+      >
+        <View className="w-full items-center">
+          <Bell
+            width={50}
+            height={50}
+            style={{ color }}
+          />
+          <Text className="text-xl font-semibold mt-2">
+            Activa las notificaciones
+          </Text>
+          <Text className="text-gray-600 mt-2 text-center">
+            Para recibir actualizaciones importantes, debes activar las
+            notificaciones en la configuración de tu dispositivo.
+          </Text>
+          <StyledPressable
+            pressableClass="mt-5"
+            text="Abrir configuraciones"
+            backgroundColor={`${color}cc`}
+            onPress={handleClickOpenSettings}
+          />
+          <StyledPressable
+            text="Más tarde"
+            textClassName="text-sm text-gray-600"
+            pressableClass="h-7 p-0 mt-2 mb-4"
+            onPress={handleClickdDismissModalize}
+          />
+        </View>
+      </Modalize>
+      <View
+        className="w-11/12 h-auto border rounded-lg"
+        style={{ borderColor: color }}
+      >
+        <View
+          className="py-6 px-5 rounded-tr-md rounded-tl-md"
+          style={{ backgroundColor: color }}
+        >
+          <Text className="text-center text-2xl text-white">
+            Votación finalizada
+          </Text>
+        </View>
+        <View className="w-full items-center gap-y-3 py-6 px-5">
+          <View className="animate-pulse">
+            <BarChart
+              width={90}
+              height={90}
+              color={color}
+            />
+          </View>
+          <Text className="text-xl text-gray-700 font-semibold text-center">
+            Calculando resultados...
+          </Text>
+          <Text className="text-gray-500 text-center">
+            La votación ha finalizado. Estamos procesando todos los votos para
+            determinar el ganador. Este proceso puede tomar algún tiempo.
+          </Text>
+
+          <View className="mt-5">
+            <StyledPressable
+              text="Notificar cuando haya un ganador"
+              backgroundColor={color}
+              textClassName="text-white"
+              onPress={checkPermissions}
+            />
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
