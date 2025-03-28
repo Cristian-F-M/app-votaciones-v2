e94e529af7e2263f2a0c2a3350eb4c021c41c581
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-28 12:20:24 -0500

    refactor: Replace custom styled Pressable with native Pressable to simplify component structure and enhance performance

--- components/StyledPressable.jsx
+++ components/StyledPressable.jsx
@@ -1,5 +1,4 @@
 import { ActivityIndicator, Pressable, Text } from 'react-native'
-import { styled } from 'nativewind'
 
 export function StyledPressable({
   backgroundColor = 'white',
@@ -11,10 +10,8 @@ export function StyledPressable({
   isLoading = false,
   disabled = false,
 }) {
-  const StyledPressable = styled(Pressable)
-
   return (
-    <StyledPressable
+    <Pressable
       className={`px-2 py-3 rounded-lg w-full active:opacity-70 flex-row justify-center items-center ${isLoading ? 'opacity-70' : ''} ${pressableClass}`}
       style={{ backgroundColor: disabled ? '#b3b6bd' : backgroundColor }}
       onPress={!isLoading ? onPress : null}
@@ -32,6 +29,6 @@ export function StyledPressable({
           color="white"
         />
       )}
-    </StyledPressable>
+    </Pressable>
   )
 }
