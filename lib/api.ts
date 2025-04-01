6026b9745484aeb28221ae35206694ab21db0bd2
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-31 22:57:42 -0500

    feat: Implement API utility functions for storage and fetching data in lib/api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -9,21 +9,30 @@ export const METHODS = {
 }
 
 export async function doFetch({
-  url = process.env.API_URL,
+  url,
   method = METHODS.GET,
   body,
   includeContentType = true,
   contentType = 'application/json',
   stringifyBody = true,
+}: {
+  url: string
+  method?: (typeof METHODS)[keyof typeof METHODS]
+  body?: Record<string, any> | FormData
+  includeContentType?: boolean
+  contentType?: string
+  stringifyBody?: boolean
 }) {
   const token = await getItemStorage({ name: 'token' })
-  const newBody = stringifyBody ? JSON.stringify(body) : body
+  let newBody: FormData | string = JSON.stringify(body)
+  if (!stringifyBody && body instanceof FormData) newBody = body
+
   let res
-  url = url.startsWith('http')
+  url = url?.startsWith('http')
     ? url
     : `${process.env.EXPO_PUBLIC_API_URL}/${url}`
 
-  const headers = {
+  const headers: RequestInit['headers'] = {
     Cookie: `token=${token?.value}`,
     'User-Agent': 'mobile',
   }
@@ -41,30 +50,39 @@ export async function doFetch({
     if (!d.ok && d.returnUrl) router.replace(d.returnUrl)
 
     return d
-  } catch (e) {
+  } catch (e: unknown) {
     const text = await res?.text()
     if (text) console.log({ text })
-    console.log({ err: e.message })
+    if (e instanceof Error) console.log({ err: e.message })
     return { ok: false, error: 'An error has ocurred' }
   }
 }
 
-export async function getItemStorage({ name }) {
-  return await JSON.parse(await AsyncStorage.getItem(name))
+export async function getItemStorage({ name }: { name: string }) {
+  const item = await AsyncStorage.getItem(name)
+  return item ? JSON.parse(item) : null
 }
 
-export async function setItemStorage({ name, value, expires = null }) {
+export async function setItemStorage({
+  name,
+  value,
+  expires = null,
+}: {
+  name: string
+  value: any
+  expires?: Date | null
+}) {
   return await AsyncStorage.setItem(
     name,
     JSON.stringify({ name, value, expires }),
   )
 }
 
-export async function removeItemStorage({ name }) {
+export async function removeItemStorage({ name }: { name: string }) {
   return await AsyncStorage.removeItem(name)
 }
 
-export async function saveConfig(key, value) {
+export async function saveConfig(key: string, value: any) {
   const settings = (await getConfigs()) || {}
   settings[key] = value
   await setItemStorage({ name: 'settings', value: settings })
@@ -72,17 +90,18 @@ export async function saveConfig(key, value) {
 
 export async function getConfigs() {
   const settigs = await AsyncStorage.getItem('settings')
+  if (!settigs) return {}
   return JSON.parse(settigs)?.value || {}
 }
 
-export async function removeConfig(key) {
+export async function removeConfig(key: string) {
   const settings = (await getConfigs()) || {}
   delete settings[key]
   await setItemStorage({ name: 'settings', value: settings })
 }
 
-export function getApiErrors(errors) {
-  const apiErrors = {}
+export function getApiErrors(errors: Record<string, any>) {
+  const apiErrors: Record<string, string> = {}
 
   Object.entries(errors).forEach(([_, v]) => {
     const { path, msg } = v[0]
@@ -92,9 +111,9 @@ export function getApiErrors(errors) {
   return apiErrors
 }
 
-export function getApiErrorsEntries(errors) {
+export function getApiErrorsEntries(errors: Record<string, any>) {
   const entries = Object.entries(errors)
-  let localyEntries = []
+  let localyEntries: [string, any][] = []
   entries.forEach(([key, value]) => {
     if (value != null) localyEntries.push([key, value])
   })
