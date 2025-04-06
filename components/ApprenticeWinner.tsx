92d03e4b65480f56b897a95e3276a71df4c3259d
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-06 14:10:26 -0500

    refactor: Remove unuse code and refreshControl in the ScrollView

--- components/ApprenticeWinner.tsx
+++ components/ApprenticeWinner.tsx
@@ -1,7 +1,5 @@
-import { RefreshControl, ScrollView, Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { CardApprenticeWinner } from './CardApprenticeWinner'
-import { useConfig } from '../context/config'
-import { findConfig } from '../lib/config'
 import { doFetch, METHODS } from '@lib/api'
 import { Screen } from './Screen'
 import { Vote } from 'types/vote'
@@ -9,12 +7,7 @@ import { useCallback, useEffect, useState } from 'react'
 import { LoaderWinner } from './LoaderWinner'
 
 export function ApprenticeWinner() {
-  const configs = useConfig()
-  const config = configs?.config || []
-  const configColor = findConfig({ configs: config, code: 'Color' })
-  const color = configColor?.value || '#5b89d6'
   const [vote, setVote] = useState<Vote>()
-  const [refreshing, setRefreshing] = useState(false)
 
   const getLastVote = useCallback(async () => {
     const res: { lastVote: Vote; ok: boolean; error?: string } = await doFetch({
@@ -25,8 +18,6 @@ export function ApprenticeWinner() {
     if (res.error || !res.ok) return
 
     const { lastVote: vote } = res
-
-    console.log({ vote })
     setVote(vote)
   }, [])
 
@@ -34,13 +25,6 @@ export function ApprenticeWinner() {
     setTimeout(() => {
       getLastVote()
     }, 500)
-  }, [getLastVote])
-
-  const onRefresh = useCallback(async () => {
-    setRefreshing(true)
-    setVote(undefined)
-    await getLastVote()
-    setRefreshing(false)
   }, [getLastVote])
 
   if (!vote) return <LoaderWinner />
@@ -56,32 +40,33 @@ export function ApprenticeWinner() {
   return (
     <Screen safeArea={false}>
       <ScrollView
-        refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-            colors={[color, 'black']}
-          />
-        }
-        className="flex mt-14 w-11/12 mx-auto py-4 pt-0 border border-gray-200 rounded-lg"
-        style={{
-          boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.2)',
+        className="flex flex-col"
+        contentContainerStyle={{
+          alignItems: 'center',
+          justifyContent: 'space-between',
         }}
       >
-        <View className="w-full justify-center items-center bg-blue-100/70 rounded-t-lg px-2 py-6">
-          <Text className="text-3xl text-center">
-            Resultados de la Votación
+        <View
+          className="flex mt-14 w-11/12 mx-auto py-4 pt-0 border border-gray-200 rounded-lg"
+          style={{
+            boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.2)',
+          }}
+        >
+          <View className="w-full justify-center items-center bg-blue-100/70 rounded-t-lg px-2 py-6">
+            <Text className="text-3xl text-center">
+              Resultados de la Votación
+            </Text>
+          </View>
+          <View className="flex mt-6 w-11/12 mx-auto">
+            <CardApprenticeWinner vote={vote} />
+          </View>
+        </View>
+        <View className="w-full flex flex-row justify-center mt-10 mb-2 self-end justify-self-end">
+          <Text className="text-center text-sm text-gray-700">
+            El Aprendiz será representante hasta el {endWinnerDateString}
           </Text>
         </View>
-        <View className="flex mt-6 w-11/12 mx-auto">
-          <CardApprenticeWinner vote={vote} />
-        </View>
       </ScrollView>
-      <View className="w-full flex flex-row justify-center mt-auto mb-2">
-        <Text className="text-center text-sm text-gray-700">
-          El Aprendiz será representante hasta el {endWinnerDateString}
-        </Text>
-      </View>
     </Screen>
   )
 }
