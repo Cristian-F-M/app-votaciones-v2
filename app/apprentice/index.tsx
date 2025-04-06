b55a66c14b0973e7fae6be81ac7d6e6c1a1c278e
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-06 14:08:16 -0500

    feat: Enhance Index component with loading state and pull-to-refresh functionality for improved data fetching experience

--- app/apprentice/index.tsx
+++ app/apprentice/index.tsx
@@ -4,14 +4,19 @@ import { ApprenticeWinner } from '../../components/ApprenticeWinner'
 import { Vote } from '../../components/Vote'
 import { ThereIsNoVote } from '../../components/ThereIsNoVote'
 import { LoadingWinner } from '../../components/LoadingWinner'
+import { LoaderWinner } from '@components/LoaderWinner'
+import { RefreshControl, ScrollView } from 'react-native'
 
 export default function Index() {
   const url = process.env.EXPO_PUBLIC_API_URL
   const [isVoteFinished, setIsVoteFinished] = useState(false)
   const [thereIsVote, setThereIsVote] = useState(false)
   const [arrivedTime, setArrivedTime] = useState(true)
+  const [loading, setLoading] = useState(true)
 
   const getLastVote = useCallback(async () => {
+    setLoading(true)
+
     doFetch({ url: `${url}/vote/`, method: METHODS.GET }).then(data => {
       if (data.error) return
 
@@ -19,11 +24,10 @@ export default function Index() {
 
       if (vote) setThereIsVote(true)
 
-      if (new Date().getTime() > new Date(vote.startDate).getTime())
-        setThereIsVote(true)
       if (new Date().getTime() > new Date(vote.endDate).getTime())
         setArrivedTime(true)
-      if (vote?.isFinished) setIsVoteFinished(true)
+      setIsVoteFinished(vote?.isFinished || false)
+      setLoading(false)
     })
   }, [url])
 
@@ -31,15 +35,28 @@ export default function Index() {
     getLastVote()
   }, [getLastVote])
 
-  const showVote = !isVoteFinished && thereIsVote && !arrivedTime
-  const showWinner = isVoteFinished && thereIsVote && arrivedTime
+  const showVote = !isVoteFinished && thereIsVote && !arrivedTime && !loading
+  const showWinner = isVoteFinished && thereIsVote && arrivedTime && !loading
+
+  const onRefresh = async () => {
+    getLastVote()
+  }
 
   return (
-    <>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={loading}
+          onRefresh={onRefresh}
+          colors={['#5b89d6', 'black']}
+        />
+      }
+    >
+      {loading && <LoaderWinner />}
       {showWinner && <ApprenticeWinner />}
       {showVote && <Vote />}
-      {!thereIsVote && <ThereIsNoVote />}
-      {arrivedTime && !isVoteFinished && <LoadingWinner />}
-    </>
+      {!thereIsVote && !loading && <ThereIsNoVote />}
+      {arrivedTime && !isVoteFinished && !loading && <LoadingWinner />}
+    </ScrollView>
   )
 }
